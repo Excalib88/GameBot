4fede8cd189d6207d8d09d81a054f9451aabe5fe
Excalib88 <rave9373@gmail.com>
2017-12-10 22:15:16 +0300

    Add log to file

--- GameBot/Logger.cs
+++ GameBot/Logger.cs
@@ -6,6 +6,7 @@ namespace BotGame
     public static class Logger
     {
         public const string TIME_FORMAT = @"yyyy-MM-dd HH:mm:ss";
+        public const string DATE_FORMAT = @"yyyy-MM-dd";
 
         #region write
 
@@ -41,10 +42,37 @@ namespace BotGame
 
         private static void WriteToLog(string textLog, ConsoleColor color, string methodName)
         {
-            DateTime dtnow = DateTime.Now;
+            DateTime currentDateTime = DateTime.Now;
+            string currentDateTimeString = currentDate.ToString(TIME_FORMAT);
+            string logMessage = String.Format("{0} [{1}] {2}", currentDateString, methodName, textLog);
+
             Console.ForegroundColor = color;
-            Console.WriteLine(dtnow.ToString(TIME_FORMAT) + " [" + methodName + "] " + textLog);
+
+            Console.WriteLine(logMessage);
+            Logger.WriteLogToFile(logMessage, currentDateTime);
+
             Console.ResetColor();
+        }
+        private async static void WriteLogToFile(string logText, DateTime currentDateTime)
+        {
+            string currentDateString = currentDateTime.ToString(DATE_FORMAT);
+            string logFileDirectory = String.Format(@"log\log{0}.txt", currentDateString);
+            UnicodeEncoding unicodeEncoding = new UnicodeEncoding();
+            byte[] logData = unicodeEncoding.GetBytes(String.Format("{0}:{1}{2}", currentDateTime.ToString(TIME_FORMAT), logText, Environment.NewLine));
+
+            try
+            {
+                using (FileStream logStream = new FileStream(logFileDirectory, FileMode.OpenOrCreate))
+                {
+                    logStream.Seek(0, SeekOrigin.End);
+                    await logStream.WriteAsync(logData, 0, logData.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
         }
     }
 }
